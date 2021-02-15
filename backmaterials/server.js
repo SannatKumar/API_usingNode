a07d62caba328327049986dceb1021dddb9cf560
Raj Kumar <interplaytheheads@gmail.com>
2021-02-15 19:54:31 +0200

    Message changed

--- backmaterials/server.js
+++ backmaterials/server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 //define a simple route
 app.get('/', (req,res) => {
-    res.json({"message": "Welcome to EasyNotes Application. Take notes quickly. Organize and keep track of your notes."})
+    res.json({"message": "Welcome to Backend of the application"})
 });
 
 //listen for requests
