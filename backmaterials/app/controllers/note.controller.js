0f5ea9c0ef245200e1a627cf7fa5352ba0e69e34
Raj Kumar <interplaytheheads@gmail.com>
2021-02-15 20:14:36 +0200

    Functions template created

---
+++ backmaterials/app/controllers/note.controller.js
@@ -0,0 +1,28 @@
+const Note = require('../models/note.model.js');
+
+//create and Save a new Note
+exports.create = (req, res) => {
+
+};
+
+//REtriev and return all notes from the database
+exports.findAll = (req, res) => {
+
+};
+
+//Find a single note with a NoteId
+exports.findOne = (req, res) => {
+
+};
+
+//Update a note with the specified noteId in the request
+exports.update = (req, res) => {
+
+};
+
+//Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+
+};
+
+
