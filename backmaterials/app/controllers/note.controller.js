dabff4cbd17c8d80ec9827b60d42b1db8d2695fe
Raj Kumar <interplaytheheads@gmail.com>
2021-02-17 13:52:53 +0200

    Retrieving single note added

--- backmaterials/app/controllers/note.controller.js
+++ backmaterials/app/controllers/note.controller.js
@@ -43,6 +43,24 @@ exports.findAll = (req, res) => {
 
 //Find a single note with a NoteId
 exports.findOne = (req, res) => {
+    Note.findById(req.params.noteId)
+    .then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({
+                message: "Note not found wit id" + req.params,noteId
+            });
+        }
+        return res.status(500).send({
+            message: "Error retrieving note with id" + req.params.noteId
+        });
+    });
 
 };
 
